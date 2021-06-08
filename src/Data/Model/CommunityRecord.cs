7a5dca69d2be02bab25b9a49405d67ea7c11d0c6
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2021-06-08 23:28:07 +0300

    Add meetup models

---
+++ src/Data/Model/CommunityRecord.cs
@@ -0,0 +1,25 @@
+﻿namespace DotNetRu.Auditor.Data.Model
+{
+    public sealed class CommunityRecord
+    {
+        public string? Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? City { get; set; }
+
+        public string? TimeZone { get; set; }
+
+        public string? VkUrl { get; set; }
+
+        public string? TwitterUrl { get; set; }
+
+        public string? TelegramChannelUrl { get; set; }
+
+        public string? TelegramChatUrl { get; set; }
+
+        public string? MeetupComUrl { get; set; }
+
+        public string? TimePadUrl { get; set; }
+    }
+}
---
+++ src/Data/Model/TalkRecord.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace DotNetRu.Auditor.Data.Model
+{
+    public sealed class TalkRecord
+    {
+        public string? Id { get; set; }
+
+        public List<string> SpeakerIds { get; } = new();
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public List<string> SeeAlsoTalkIds { get; } = new();
+
+        public string? CodeUrl { get; set; }
+
+        public string? SlidesUrl { get; set; }
+
+        public string? VideoUrl { get; set; }
+    }
+}

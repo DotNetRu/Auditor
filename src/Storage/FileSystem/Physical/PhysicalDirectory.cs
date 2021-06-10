902be7e468b3b941e27932c28fa8c83234c1ce3c
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2021-06-10 23:57:22 +0300

    Add physical file system

---
+++ src/Storage/FileSystem/Physical/PhysicalDirectory.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DotNetRu.Auditor.Storage.FileSystem.Physical
+{
+    public class PhysicalDirectory : FileSystemEntry, IDirectory
+    {
+        public PhysicalDirectory(string fullName)
+            : base(fullName, Directory.Exists(fullName))
+        {
+        }
+
+        public Task<IDirectory> GetDirectoryInfoAsync(string subPath)
+        {
+            if (!Exists)
+            {
+                return NotFoundDirectory.ToTask(subPath);
+            }
+
+            var fullDirectoryName = GetFullPath(subPath);
+            var directory = new PhysicalDirectory(fullDirectoryName);
+            return Task.FromResult<IDirectory>(directory);
+        }
+
+        public Task<IFile> GetFileInfoAsync(string subPath)
+        {
+            if (!Exists)
+            {
+                return NotFoundFile.ToTask(subPath);
+            }
+
+            var fullFileName = GetFullPath(subPath);
+            var file = new PhysicalFile(fullFileName);
+            return Task.FromResult<IFile>(file);
+        }
+
+        public async IAsyncEnumerable<IDirectory> EnumerateDirectoriesAsync()
+        {
+            if (!Exists)
+            {
+                yield break;
+            }
+
+            await Task.Yield();
+
+            foreach (var directoryFullName in Directory.EnumerateDirectories(FullName))
+            {
+                yield return new PhysicalDirectory(directoryFullName);
+            }
+        }
+
+        public async IAsyncEnumerable<IFile> EnumerateFilesAsync()
+        {
+            if (!Exists)
+            {
+                yield break;
+            }
+
+            await Task.Yield();
+
+            foreach (var fileFullName in Directory.EnumerateFiles(FullName))
+            {
+                yield return new PhysicalFile(fileFullName);
+            }
+        }
+    }
+}
---
+++ src/Storage/FileSystem/Physical/PhysicalFile.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace DotNetRu.Auditor.Storage.FileSystem.Physical
+{
+    public sealed class PhysicalFile : FileSystemEntry, IFile
+    {
+        public PhysicalFile(string fullName)
+            : base(fullName, File.Exists(fullName))
+        {
+        }
+
+        public Stream OpenForRead()
+        {
+            if (!Exists)
+            {
+                throw NotFoundFile.ToException(FullName);
+            }
+
+            return File.OpenRead(FullName);
+        }
+
+        public Stream OpenForWrite()
+        {
+            if (!Exists)
+            {
+                throw NotFoundFile.ToException(FullName);
+            }
+
+            return File.OpenWrite(FullName);
+        }
+    }
+}

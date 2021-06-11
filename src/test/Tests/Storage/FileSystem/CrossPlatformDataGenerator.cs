343d32a66f127b4409e609859e3e22c03d2c4ee6
zetroot <zetroot@gmail.com>
2021-06-11 10:40:19 +0300

    refactored test data to meet crossplatform path requirements

---
+++ src/test/Tests/Storage/FileSystem/CrossPlatformDataGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace DotNetRu.Auditor.Tests.Storage.FileSystem
+{
+    public class CrossPlatformDataGenerator
+    {
+        private static readonly string filesystemRoot;
+
+        static CrossPlatformDataGenerator()
+        {
+            filesystemRoot = Path.GetPathRoot(Directory.GetCurrentDirectory()) ?? string.Empty;
+        }
+
+        public static IEnumerable<object[]> GetDataForFullPathTest()
+        {
+            yield return new object[] {filesystemRoot, "Abc", Path.Combine(filesystemRoot, "Abc")};
+            yield return new object[] {Path.Combine(filesystemRoot, "1"), @".\Abc", Path.Combine(filesystemRoot, "1", "Abc")};
+        }
+    }
+}

e82f548b846b5a9020a5eb6a14ab6b1a798db705
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2021-06-20 19:41:12 +0300

    Extract writable file

--- src/Storage/FileSystem/NotFoundFile.cs
+++ src/Storage/FileSystem/NotFoundFile.cs
@@ -1,22 +1,31 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Threading.Tasks;
 
 namespace DotNetRu.Auditor.Storage.FileSystem
 {
-    public class NotFoundFile : FileSystemEntry, IFile
+    public class NotFoundFile : FileSystemEntry, IWritableFile
     {
         public NotFoundFile(string fullName)
             : base(fullName, false)
         {
         }
 
-        public static ValueTask<IFile> ToTask(string fullName) => ValueTask.FromResult<IFile>(new NotFoundFile(fullName));
+        public static ValueTask<IFile> AsTask(string fullName) => ValueTask.FromResult<IFile>(new NotFoundFile(fullName));
 
-        public static Exception ToException(string fullName) => new FileNotFoundException($"Could not find file: {fullName}", fullName);
+        public static Exception AsException(string fullName) => new FileNotFoundException($"Could not find file: {fullName}", fullName);
 
-        public ValueTask<Stream> OpenForReadAsync() => throw ToException(FullName);
+        public ValueTask<Stream> OpenForReadAsync() => throw AsException(FullName);
 
-        public ValueTask<Stream> OpenForWriteAsync() => throw ToException(FullName);
+        public ValueTask<bool> RequestWriteAccessAsync([NotNullWhen(true)] out IWritableFile? writableFile)
+        {
+            writableFile = default;
+            return ValueTask.FromResult(false);
+        }
+
+        public ValueTask<Stream> OpenForWriteAsync() => throw AsException(FullName);
+
+        public ValueTask<bool> DeleteAsync() => ValueTask.FromResult(false);
     }
 }

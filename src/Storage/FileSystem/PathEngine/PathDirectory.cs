c9b809cc7acdeb2363e30c115b37091d20198a35
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2021-07-05 21:15:39 +0300

    Migrate to Path based Directory

---
+++ src/Storage/FileSystem/PathEngine/PathDirectory.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DotNetRu.Auditor.Storage.FileSystem.PathEngine
+{
+    internal sealed class PathDirectory : FileSystemEntry, IDirectory
+    {
+        private readonly IFileSystem fileSystem;
+
+        public PathDirectory(string path, IFileSystem fileSystem)
+            : base(path)
+        {
+            this.fileSystem = fileSystem;
+        }
+
+        public override ValueTask<bool> ExistsAsync() => fileSystem.DirectoryExistsAsync(FullName);
+
+        public IDirectory GetDirectory(string childDirectoryName)
+        {
+            var fullChildName = GetFullChildPath(childDirectoryName);
+            var directory = new PathDirectory(fullChildName, fileSystem);
+            return directory;
+        }
+
+        public IFile GetFile(string childFileName)
+        {
+            var fullChildName = GetFullChildPath(childFileName);
+            var file = new PathFile(fullChildName, fileSystem);
+            return file;
+        }
+
+        public async IAsyncEnumerable<IDirectory> EnumerateDirectoriesAsync()
+        {
+            await foreach (var directoryFullName in fileSystem.EnumerateDirectoriesAsync(FullName))
+            {
+                yield return new PathDirectory(directoryFullName, fileSystem);
+            }
+        }
+
+        public async IAsyncEnumerable<IFile> EnumerateFilesAsync()
+        {
+            await foreach (var fileFullName in fileSystem.EnumerateFilesAsync(FullName))
+            {
+                yield return new PathFile(fileFullName, fileSystem);
+            }
+        }
+    }
+}

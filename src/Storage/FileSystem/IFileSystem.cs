4a2481bc1cc28113868c30be4de0873fc5e3dbd8
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2021-07-05 21:13:57 +0300

    Extract file system adapter

---
+++ src/Storage/FileSystem/IFileSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DotNetRu.Auditor.Storage.FileSystem
+{
+    /// <remarks>
+    /// In fact, most operating systems do not support asynchronous enumeration of file system contents.
+    /// This is why there is no asynchronous API in the official BCL. But we have a Blazor consumer that
+    /// throws an exception when using synchronization operations, and a GutHub HTTP API implementation
+    /// that can perform real asynchronous operations.
+    ///
+    /// To respect I/O operations, we decided to use asynchronous abstractions. But we assume that real
+    /// asynchronous operations will be rare, so we decided to use ValueTask.
+    /// </remarks>>
+    internal interface IFileSystem
+    {
+        IAsyncEnumerable<string> EnumerateDirectoriesAsync(string path);
+
+        IAsyncEnumerable<string> EnumerateFilesAsync(string path);
+
+        ValueTask<bool> DirectoryExistsAsync(string path);
+
+        ValueTask<bool> FileExistsAsync(string path);
+
+        ValueTask<bool> RequestWriteAccessForFileAsync(string path);
+
+        ValueTask<Stream> OpenFileForReadAsync(string path);
+
+        ValueTask<Stream> OpenFileForWriteAsync(string path);
+
+        ValueTask<bool> DeleteFileAsync(string path);
+    }
+}
---
+++ src/Storage/FileSystem/PhysicalFileSystem.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using DotNetRu.Auditor.Storage.FileSystem.PathEngine;
+
+namespace DotNetRu.Auditor.Storage.FileSystem
+{
+    public sealed class PhysicalFileSystem : IFileSystem
+    {
+        // HACK: Resolve cross-platform root directory
+        public static readonly string RootPath = Path.GetPathRoot(Environment.CurrentDirectory) ?? throw new InvalidOperationException("Unknown root directory");
+
+        public static IDirectory ForDirectory(string path)
+        {
+            var fileSystem = new PhysicalFileSystem();
+            return fileSystem.GetDirectory(path);
+        }
+
+        public IDirectory GetDirectory(string path)
+        {
+            var directory = new PathDirectory(path, this);
+            return directory;
+        }
+
+        async IAsyncEnumerable<string> IFileSystem.EnumerateDirectoriesAsync(string path)
+        {
+            var exists = Directory.Exists(path);
+            if (!exists)
+            {
+                yield break;
+            }
+
+            await Task.Yield();
+
+            foreach (var directoryFullName in Directory.EnumerateDirectories(path))
+            {
+                yield return directoryFullName;
+            }
+        }
+
+        async IAsyncEnumerable<string> IFileSystem.EnumerateFilesAsync(string path)
+        {
+            var exists = Directory.Exists(path);
+            if (!exists)
+            {
+                yield break;
+            }
+
+            await Task.Yield();
+
+            foreach (var fileFullName in Directory.EnumerateFiles(path))
+            {
+                yield return fileFullName;
+            }
+        }
+
+        ValueTask<bool> IFileSystem.DirectoryExistsAsync(string path)
+        {
+            var exists = Directory.Exists(path);
+            return ValueTask.FromResult(exists);
+        }
+
+        ValueTask<bool> IFileSystem.FileExistsAsync(string path)
+        {
+            var exists = File.Exists(path);
+            return ValueTask.FromResult(exists);
+        }
+
+        ValueTask<bool> IFileSystem.RequestWriteAccessForFileAsync(string path)
+        {
+            // TODO: Test real file system permissions
+            return ValueTask.FromResult(true);
+        }
+
+        ValueTask<Stream> IFileSystem.OpenFileForReadAsync(string path)
+        {
+            var exists = File.Exists(path);
+            if (!exists)
+            {
+                throw FileNotFound(path);
+            }
+
+            var inputStream = File.OpenRead(path);
+            return ValueTask.FromResult<Stream>(inputStream);
+        }
+
+        ValueTask<Stream> IFileSystem.OpenFileForWriteAsync(string path)
+        {
+            EnsureParentDirectoryExists(path);
+
+            var outputStream = File.OpenWrite(path);
+            return ValueTask.FromResult<Stream>(outputStream);
+        }
+
+        ValueTask<bool> IFileSystem.DeleteFileAsync(string path)
+        {
+            var exists = File.Exists(path);
+            if (!exists)
+            {
+                return ValueTask.FromResult(false);
+            }
+
+            try
+            {
+                File.Delete(path);
+            }
+            catch (Exception)
+            {
+                // Result will be checked later
+            }
+
+            exists = File.Exists(path);
+            return ValueTask.FromResult(!exists);
+        }
+
+        private static void EnsureParentDirectoryExists(string path)
+        {
+            var parentDirectoryPath = Path.GetDirectoryName(path);
+            if (parentDirectoryPath != null && !Directory.Exists(parentDirectoryPath))
+            {
+                Directory.CreateDirectory(parentDirectoryPath);
+            }
+        }
+
+        private static FileNotFoundException FileNotFound(string path) => new($"Could not find file: {path}", path);
+    }
+}

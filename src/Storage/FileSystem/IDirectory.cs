1c602c32ec7c147833fc2f78876adb362a22b775
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2021-07-05 21:15:39 +0300

    Migrate to Path based Directory

--- src/Storage/FileSystem/IDirectory.cs
+++ src/Storage/FileSystem/IDirectory.cs
@@ -1,22 +1,12 @@
 ﻿using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace DotNetRu.Auditor.Storage.FileSystem
 {
-    /// <remarks>
-    /// In fact, most operating systems do not support asynchronous enumeration of file system contents.
-    /// This is why there is no asynchronous API in the official BCL. But we have a Blazor consumer that
-    /// throws an exception when using synchronization operations, and a GutHub HTTP API implementation
-    /// that can perform real asynchronous operations.
-    ///
-    /// To respect I/O operations, we decided to use asynchronous abstractions. But we assume that real
-    /// asynchronous operations will be rare, so we decided to use ValueTask.
-    /// </remarks>>
     public interface IDirectory : IFileSystemEntry
     {
-        ValueTask<IDirectory> GetDirectoryAsync(string childDirectoryName);
+        IDirectory GetDirectory(string childDirectoryName);
 
-        ValueTask<IFile> GetFileAsync(string childFileName);
+        IFile GetFile(string childFileName);
 
         IAsyncEnumerable<IDirectory> EnumerateDirectoriesAsync();
 

bd545e2ff6b5fcaac62c03f67745f5c5f527324c
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2021-07-28 00:22:43 +0300

    Add Session

---
+++ src/Storage/Collections/SessionOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNetRu.Auditor.Storage.Collections
+{
+    public sealed class SessionOptions
+    {
+        // TDO: Add change tracking
+        public bool TrackChanges { get; set; }
+
+        // TDO: Cache deserialized records
+        public bool CacheRecords { get; set; } = true;
+    }
+}
---
+++ src/Storage/Dictionary.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace DotNetRu.Auditor.Storage
+{
+    internal static class Dictionary
+    {
+        public static IReadOnlyDictionary<TKey, TValue> Empty<TKey, TValue>()
+            where TKey : notnull
+        {
+            return EmptyDictionary<TKey, TValue>.Instance;
+        }
+
+        private static class EmptyDictionary<TKey, TValue>
+            where TKey : notnull
+        {
+            public static readonly IReadOnlyDictionary<TKey, TValue> Instance = new Dictionary<TKey, TValue>(0);
+        }
+    }
+}

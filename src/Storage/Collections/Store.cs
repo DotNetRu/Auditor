5689fdc961a973ee2a9134d8fa9f0d2f32e1fe81
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2021-07-28 00:24:29 +0300

    Add Audit Store

---
+++ src/Storage/Collections/Store.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using DotNetRu.Auditor.Data;
+using DotNetRu.Auditor.Data.Xml;
+
+namespace DotNetRu.Auditor.Storage.Collections
+{
+    // TDO: Add integration tests
+    internal sealed class Store : IStore
+    {
+        private readonly IDataSerializerFactory serializerFactory = new XmlDataSerializerFactory();
+        private readonly StoreOptions options;
+        private readonly IReadOnlyDictionary<string, Collection> collections;
+
+        public Store(StoreOptions options, IReadOnlyList<Collection> collections)
+        {
+            this.options = options;
+            this.collections = collections.ToDictionary(collection => collection.Name);
+        }
+
+        public ISession OpenSession(SessionOptions? sessionOptions = null)
+        {
+            sessionOptions ??= new SessionOptions();
+            return new Session(sessionOptions, TryGetCollection, serializerFactory);
+        }
+
+        private bool TryGetCollection(Type collectionType, [NotNullWhen(true)] out Collection? collection)
+        {
+            var collectionName = options.MapCollectionName(collectionType);
+            return collections.TryGetValue(collectionName, out collection);
+        }
+    }
+}
---
+++ test/UnitTests/Storage/Collections/StoreOptionsTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DotNetRu.Auditor.Data.Model;
+using DotNetRu.Auditor.Storage.Collections;
+using DotNetRu.Auditor.UnitTests.Data.Xml;
+using Xunit;
+
+namespace DotNetRu.Auditor.UnitTests.Storage.Collections
+{
+    public sealed class StoreOptionsTest
+    {
+        private readonly StoreOptions options;
+
+        public StoreOptionsTest()
+        {
+            options = new StoreOptions();
+        }
+
+        [Theory]
+        [MemberData(nameof(RecordsWithName))]
+        public void ShouldMapCollectionName(Type recordType, string expectedCollectionName)
+        {
+            // Act
+            var actualCollectionName = options.MapCollectionName(recordType);
+
+            // Assert
+            Assert.Equal(expectedCollectionName, actualCollectionName);
+        }
+
+        [Fact]
+        public void ShouldCheckAllRecords()
+        {
+            // Arrange
+            var allRecordTypes = XmlDataSerializerFactoryTest.ModelTypes;
+
+            // Act
+            var namedRecordType = NamedRecords.Select(pair => pair.Key);
+
+            // Assert
+            Assert.Equal(allRecordTypes, namedRecordType);
+        }
+
+        public static TheoryData<Type, string> RecordsWithName => NamedRecords.ToTheoryData();
+
+        private static IReadOnlyDictionary<Type, string> NamedRecords => new Dictionary<Type, string>
+        {
+            { typeof(CommunityRecord), "communities" },
+            { typeof(FriendRecord), "friends" },
+            { typeof(MeetupRecord), "meetups" },
+            { typeof(SpeakerRecord), "speakers" },
+            { typeof(TalkRecord), "talks" },
+            { typeof(VenueRecord), "venues" }
+        };
+    }
+}

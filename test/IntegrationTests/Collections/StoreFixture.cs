2b40d2b68d61f870c39598119b5e041ebdc1bcc5
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2021-07-28 00:25:18 +0300

    Add Store integration test

---
+++ test/IntegrationTests/Collections/StoreFixture.cs
@@ -0,0 +1,69 @@
+﻿using System.Threading.Tasks;
+using DotNetRu.Auditor.Data;
+using DotNetRu.Auditor.Data.Model;
+using DotNetRu.Auditor.Data.Xml;
+using DotNetRu.Auditor.Storage;
+using DotNetRu.Auditor.Storage.FileSystem;
+using DotNetRu.Auditor.Storage.IO;
+using Xunit;
+
+namespace DotNetRu.Auditor.IntegrationTests.Collections
+{
+    [CollectionDefinition(Name)]
+    public sealed class StoreFixture : ICollectionFixture<StoreFixture>
+    {
+        public const string Name = nameof(StoreFixture);
+
+        public StoreFixture()
+        {
+            var memory = MemoryFileSystem.ForDirectory(AbsolutePath.Root.Child("memory").FullName);
+
+            Storage = InitializeAsync(memory).GetAwaiter().GetResult();
+        }
+
+        public IStore Storage { get; }
+
+        private static async Task<IStore> InitializeAsync(IDirectory root)
+        {
+            var community = new CommunityRecord
+            {
+                Id = "AllDotNet",
+                Name = "AllDotNet Community"
+            };
+
+            var serializerFactory = new XmlDataSerializerFactory();
+            var serializer = serializerFactory.Create<CommunityRecord>();
+
+            await CreateFileAsync(root, community, serializer).ConfigureAwait(false);
+
+            var store = await AuditStore.OpenAsync(root);
+            return store;
+        }
+
+        private static async Task CreateFileAsync(
+            IDirectory root,
+            CommunityRecord community,
+            IDataSerializer<CommunityRecord> serializer)
+        {
+            if (community.Id == null)
+            {
+                return;
+            }
+
+            var file = root.GetDirectory("communities").GetFile(community.Id + ".xml");
+            var fileExists = await file.ExistsAsync().ConfigureAwait(false);
+            Assert.False(fileExists);
+
+            var writableFile = await file.RequestWriteAccessAsync().ConfigureAwait(false);
+            writableFile = AssertEx.NotNull(writableFile);
+
+            await using (var fileStream = await writableFile.OpenForWriteAsync().ConfigureAwait(false))
+            {
+                await serializer.SerializeAsync(fileStream, community).ConfigureAwait(false);
+            }
+
+            fileExists = await file.ExistsAsync().ConfigureAwait(false);
+            Assert.True(fileExists);
+        }
+    }
+}
---
+++ test/IntegrationTests/Collections/StoreTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using DotNetRu.Auditor.Data.Model;
+using DotNetRu.Auditor.Storage;
+using Xunit;
+
+namespace DotNetRu.Auditor.IntegrationTests.Collections
+{
+    // TDO: More tests after completion of Modification feature
+    [Collection(StoreFixture.Name)]
+    public sealed class StoreTest
+    {
+        private readonly ISession session;
+
+        public StoreTest(StoreFixture fixture)
+        {
+            session = fixture.Storage.OpenSession();
+        }
+
+        [Fact]
+        public async Task ShouldReadAllCommunities()
+        {
+            // Act
+            var communities = await session.QueryAsync<CommunityRecord>().ToListAsync().ConfigureAwait(false);
+
+            // Assert
+            var singleCommunity = Assert.Single(communities);
+            var community = AssertEx.NotNull(singleCommunity);
+
+            Assert.Equal("AllDotNet", community.Id);
+            Assert.Equal("AllDotNet Community", community.Name);
+        }
+    }
+}

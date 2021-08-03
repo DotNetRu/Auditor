b9ab190749b27cf944ebebc91a8591abc86cd217
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2021-08-03 21:45:02 +0300

    Add session identity map

--- src/Storage/Collections/Session.cs
+++ src/Storage/Collections/Session.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Threading.Tasks;
 using DotNetRu.Auditor.Data;
+using DotNetRu.Auditor.Storage.Collections.Changes;
+
+using IdentityMap = System.Object;
 
 namespace DotNetRu.Auditor.Storage.Collections
 {
+    // TDO: Add integration tests
     internal sealed class Session : ISession
     {
         public delegate bool CollectionResolver(Type collectionType, [NotNullWhen(true)] out Collection? collection);
@@ -13,6 +19,7 @@ namespace DotNetRu.Auditor.Storage.Collections
         private readonly SessionOptions options;
         private readonly CollectionResolver tryResolveCollection;
         private readonly IDocumentSerializerFactory serializerFactory;
+        private readonly ConcurrentDictionary<Type, IdentityMap> registry = new();
 
         public Session(
             SessionOptions options,
@@ -24,52 +31,93 @@ namespace DotNetRu.Auditor.Storage.Collections
             this.serializerFactory = serializerFactory;
         }
 
-        public Task<T?> LoadAsync<T>(string id)
+        public async Task<T?> LoadAsync<T>(string id)
             where T : IDocument
         {
+            var map = GetIdentityMap<T>();
+            if (map.TryResolveDocument(id, out var cachedDocument))
+            {
+                return cachedDocument;
+            }
+
             if (!tryResolveCollection(typeof(T), out var collection))
             {
-                return Task.FromResult<T?>(default);
+                return default;
             }
 
             var serializer = serializerFactory.Create<T>();
-            return collection.LoadAsync(id, serializer.DeserializeAsync);
+
+            var document = await collection.LoadAsync(id, serializer.DeserializeAsync);
+
+            if (document?.Id != null)
+            {
+                var registeredDocument = map.RegisterDocument(document);
+                return registeredDocument;
+            }
+
+            return default;
         }
 
         public async Task<IReadOnlyDictionary<string, T>> LoadAsync<T>(IReadOnlyList<string> ids)
             where T : IDocument
         {
+            var map = GetIdentityMap<T>();
+            var cachedDocuments = map.GetDocuments(ids);
+            if (cachedDocuments.Count == ids.Count)
+            {
+                return cachedDocuments;
+            }
+
             if (!tryResolveCollection(typeof(T), out var collection))
             {
                 return Dictionary.Empty<string, T>();
             }
 
             var serializer = serializerFactory.Create<T>();
-            var documents = new Dictionary<string, T>(ids.Count);
+            var documents = new Dictionary<string, T>(cachedDocuments);
 
-            foreach (var id in ids)
+            foreach (var id in ids.Except(documents.Keys))
             {
                 var document = await collection.LoadAsync(id, serializer.DeserializeAsync).ConfigureAwait(false);
-                if (document != null && document.Id != null)
+
+                if (document?.Id != null)
                 {
-                    documents.Add(document.Id, document);
+                    var registeredDocument = map.RegisterDocument(document);
+                    if (registeredDocument.Id != null)
+                    {
+                        documents.Add(registeredDocument.Id, registeredDocument);
+                    }
                 }
             }
 
             return documents;
         }
 
-        public IAsyncEnumerable<T> QueryAsync<T>()
+        public async IAsyncEnumerable<T> QueryAsync<T>()
             where T : IDocument
         {
             if (!tryResolveCollection(typeof(T), out var collection))
             {
-                return AsyncEnumerable.Empty<T>();
+                yield break;
             }
 
+            // TODO: Perf: Check all document IDs in the map before query
+            var map = GetIdentityMap<T>();
+
             var serializer = serializerFactory.Create<T>();
-            var documents = collection.QueryAsync(serializer.DeserializeAsync);
-            return documents;
+
+            await foreach (var document in collection.QueryAsync(serializer.DeserializeAsync).ConfigureAwait(false))
+            {
+                if (document?.Id != null)
+                {
+                    var registeredDocument = map.RegisterDocument(document);
+                    yield return registeredDocument;
+                }
+            }
         }
+
+        private IdentityMap<T> GetIdentityMap<T>()
+            where T : IDocument =>
+            (IdentityMap<T>)registry.GetOrAdd(typeof(T), _ => new IdentityMap<T>());
     }
 }
---
+++ test/UnitTests/Storage/Collections/Changes/IdentityMapTest.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+using DotNetRu.Auditor.Data;
+using DotNetRu.Auditor.Storage.Collections.Changes;
+using Xunit;
+
+namespace DotNetRu.Auditor.UnitTests.Storage.Collections.Changes
+{
+    public sealed class IdentityMapTest
+    {
+        private const string KnownId = nameof(KnownId);
+        private readonly IdentityMap<Secret> map = new();
+
+        [Fact]
+        public void ShouldReturnTheSameInstanceWhenRegisterTwice()
+        {
+            // Arrange
+            var document = new Secret(KnownId);
+
+            // Act
+            var firstRegistered = map.RegisterDocument(document);
+            var secondRegistered = map.RegisterDocument(document);
+
+            // Assert
+            Assert.Same(firstRegistered, secondRegistered);
+        }
+
+        [Fact]
+        public void ShouldReturnTheSameInstanceWhenRegistered()
+        {
+            // Arrange
+            var document = new Secret(KnownId);
+            var registeredDocument = map.RegisterDocument(document);
+
+            // Act
+            var found = map.TryResolveDocument(KnownId, out var resolvedDocument);
+
+            // Assert
+            Assert.True(found);
+            Assert.Same(registeredDocument, resolvedDocument);
+        }
+
+        [Fact]
+        public void ShouldReturnDefaultWhenNotRegistered()
+        {
+            // Arrange
+            const string unknownId = nameof(ShouldReturnDefaultWhenNotRegistered);
+
+            // Act
+            var found = map.TryResolveDocument(unknownId, out var resolvedDocument);
+
+            // Assert
+            Assert.False(found);
+            Assert.Null(resolvedDocument);
+        }
+
+        [Fact]
+        public void ShouldReturnMany()
+        {
+            // Arrange
+            var id1 = KnownId + 1;
+            var id2 = KnownId + 2;
+            var id3 = KnownId + 3;
+            var registeredDocument1 = map.RegisterDocument(new Secret(id1));
+            var registeredDocument2 = map.RegisterDocument(new Secret(id2));
+            var ids = new[] { id2, id1, id3 };
+
+            // Act
+            var documents = map.GetDocuments(ids);
+
+            // Assert
+            Assert.Equal(2, documents.Count);
+
+            Assert.Same(registeredDocument1, documents[id1]);
+            Assert.Same(registeredDocument2, documents[id2]);
+
+            Assert.DoesNotContain(id3, documents.Keys);
+        }
+
+        [Fact]
+        public void ShouldReturnChangedDocuments()
+        {
+            // Arrange
+            var id1 = KnownId + 1;
+            var id2 = KnownId + 2;
+            var id3 = KnownId + 3;
+            var id4 = KnownId + 4;
+
+            map.RegisterDocument(new Secret(id1));
+            var registeredDocument2 = map.RegisterDocument(new Secret(id2));
+            map.RegisterDocument(new Secret(id3));
+            var registeredDocument4 = map.RegisterDocument(new Secret(id4));
+
+            // Act
+            registeredDocument2.Name += " (Modified 2)";
+            registeredDocument4.Name += " (Modified 4)";
+
+            var changedDocuments = map.GetChangedDocuments();
+
+            // Assert
+            Assert.Equal(2, changedDocuments.Count);
+
+            var modifiedDocument2 = Assert.Single(changedDocuments.Where(d => d.Id == id2));
+            Assert.Same(registeredDocument2, modifiedDocument2);
+
+            var modifiedDocument4 = Assert.Single(changedDocuments.Where(d => d.Id == id4));
+            Assert.Same(registeredDocument4, modifiedDocument4);
+        }
+
+        private sealed class Secret : IDocument
+        {
+            public Secret(string id)
+            {
+                Id = id;
+                Name = Id;
+            }
+
+            public string? Id { get; }
+            public string? Name { get; set; }
+
+            public int GetContentChecksum() => HashCode.Combine(Id, Name);
+        }
+    }
+}

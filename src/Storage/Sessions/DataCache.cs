7ee17f0559029ec9b02ba05b44e0a3e268fcfab1
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2021-11-06 22:35:43 +0300

    Extract cacheable session

---
+++ src/Storage/Sessions/DataCache.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using DotNetRu.Auditor.Data;
+
+namespace DotNetRu.Auditor.Storage.Sessions
+{
+    internal sealed class DataCache : IDataCache
+    {
+        private readonly ConcurrentDictionary<Type, IIdentityMap> identityMaps = new();
+
+        public IdentityMap<T> GetMap<T>()
+            where T : IDocument =>
+            (IdentityMap<T>)identityMaps.GetOrAdd(typeof(T), _ => new IdentityMap<T>());
+
+        public IReadOnlyList<IIdentityMap> GetAllMaps() => identityMaps.Values.ToList();
+    }
+}

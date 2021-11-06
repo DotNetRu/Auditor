832797535a7e84118a1ca090f85b3d036cf01472
Anatoly Kulakov <anatoly.kulakov@outlook.com>
2021-11-06 22:39:31 +0300

    Add modification workflow for documents

--- src/Storage/Collections/Collection.cs
+++ src/Storage/Collections/Collection.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using DotNetRu.Auditor.Data;
@@ -24,14 +24,14 @@ namespace DotNetRu.Auditor.Storage.Collections
 
         public async Task<T?> LoadAsync(string id)
         {
-            var indexFile = GetIndexFileAsync(id);
+            var indexFile = GetIndexFile(id);
             var exists = await indexFile.ExistsAsync().ConfigureAwait(false);
             if (!exists)
             {
                 return default;
             }
 
-            var document = await DeserializeIndex(indexFile).ConfigureAwait(false);
+            var document = await DeserializeIndexAsync(indexFile).ConfigureAwait(false);
             return document;
         }
 
@@ -39,7 +39,7 @@ namespace DotNetRu.Auditor.Storage.Collections
         {
             await foreach (var indexFile in EnumerateIndexFilesAsync().ConfigureAwait(false))
             {
-                var document = await DeserializeIndex(indexFile).ConfigureAwait(false);
+                var document = await DeserializeIndexAsync(indexFile).ConfigureAwait(false);
 
                 if (document != null)
                 {
@@ -48,15 +48,35 @@ namespace DotNetRu.Auditor.Storage.Collections
             }
         }
 
-        protected abstract IFile GetIndexFileAsync(string id);
+        public Task WriteAsync(T document)
+        {
+            var id = document.Id ?? throw new ArgumentException("Can't write document without identity");
+            var indexFile = GetIndexFile(id);
+
+            return SerializeIndexAsync(indexFile, document);
+        }
+
+        protected abstract IFile GetIndexFile(string id);
 
         protected abstract IAsyncEnumerable<IFile> EnumerateIndexFilesAsync();
 
-        private async Task<T?> DeserializeIndex(IFile indexFile)
+        private async Task<T?> DeserializeIndexAsync(IFile indexFile)
         {
             await using var indexStream = await indexFile.OpenForReadAsync().ConfigureAwait(false);
             var document = await serializer.DeserializeAsync(indexStream).ConfigureAwait(false);
             return document;
+        }
+
+        private async Task SerializeIndexAsync(IFile indexFile, T document)
+        {
+            var writableFile = await indexFile.RequestWriteAccessAsync().ConfigureAwait(false);
+            if (writableFile == null)
+            {
+                throw new InvalidOperationException($"Can't write to «{indexFile.FullName}» file");
+            }
+
+            await using var indexStream = await writableFile.OpenForWriteAsync().ConfigureAwait(false);
+            await serializer.SerializeAsync(indexStream, document).ConfigureAwait(false);
         }
     }
 }
